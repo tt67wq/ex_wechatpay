36d8c5f8321a05aa77b117d7cdb3fff7497da5b7
tt67wq <tt67wq@aliyun.com>
2023-06-17 21:27:35 +0800

    feat: first commit

---
+++ lib/ex_wechatpay.ex
@@ -0,0 +1,18 @@
+defmodule ExWechatpay do
+  @moduledoc """
+  Documentation for `ExWechatpay`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExWechatpay.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_wechatpay_test.exs
@@ -0,0 +1,8 @@
+defmodule ExWechatpayTest do
+  use ExUnit.Case
+  doctest ExWechatpay
+
+  test "greets the world" do
+    assert ExWechatpay.hello() == :world
+  end
+end
