5ecb7ecd312a6c48ad2234bbaed455dc86f72d85
wanqiang <wanqiang@shizhuang-inc.com>
2024-01-29 18:16:29 +0800

    Update version to 0.1.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExWechatpay.MixProject do
   use Mix.Project
 
   @name "ex_wechatpay"
-  @version "0.1.3"
+  @version "0.1.4"
   @repo_url "https://github.com/tt67wq/ex_wechatpay"
   @description "Elixir SDK of wechat pay"
 
