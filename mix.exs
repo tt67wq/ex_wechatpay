d827bc240a35dab5d3dd505ae3e874321c38e3cb
tt67wq <tt67wq@aliyun.com>
2026-05-19 22:31:54 +0800

    chore(deps): 升相依套件版本

--- mix.exs
+++ mix.exs
@@ -35,10 +35,10 @@ defmodule ExWechatpay.MixProject do
   defp deps do
     [
       {:nimble_options, "~> 1.1"},
-      {:finch, "~> 0.20", optional: true},
+      {:finch, "~> 0.22", optional: true},
       {:jason, "~> 1.4", optional: true},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.38", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
       {:styler, "~> 0.11", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
